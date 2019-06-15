5cf6910241bcd85028cb37dfc722483697539ac1
Milos Pejovic <milos1pejovic@gmail.com>
2019-06-15 10:13:09 +0200

    Initial commit

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+
+define('DB_NAME', 'bgt');
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+
+define('DS', DIRECTORY_SEPARATOR);
---
+++ index.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <script
+  src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
+  integrity="sha256-pasqAKBDmFT4eHoN2ndd6lN370kFiGUFyTiUHWhU7k8="
+  crossorigin="anonymous"></script>
+</head>
+<body>
+<header>
+<?php 
+require_once 'config.php';
+?>
+</header>
+    
+<main>
+
+    
+    
+</main>    
+    
+<footer>
+    <script src="js/main.js"></script>
+</footer>
+    
+</body>
+</html>
